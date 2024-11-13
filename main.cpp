7f82253bcbdd98d357753abf9e38c77c44d16ba6
Nunzia Lomonte <nunzia.lomonte@uniba.it>
2024-11-13 17:57:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,6 +22,10 @@ int main() {
     std::cout << "Dopo la cancellazione, elemento alla posizione 1: " << listaInt.leggilista(1) << std::endl;
     std::cout << "Dopo la cancellazione, elemento alla posizione 2: " << listaInt.leggilista(2) << std::endl;
 
+    listaInt.inslista(50, 3);
+    listaInt.inslista(-9, 4);
+    listaInt.inslista(1, 5);
+    
     // Creazione di una lista di stringhe
     ListaVettore<std::string> listaString;
     
@@ -33,5 +37,11 @@ int main() {
     std::cout << "Elemento alla posizione 1: " << listaString.leggilista(1) << std::endl;
     std::cout << "Elemento alla posizione 2: " << listaString.leggilista(2) << std::endl;
 
+    std::cout << "Elemento massimo: " << listaInt.massimo() << std::endl;
+    std::cout << "Elemento minimo: " << listaInt.minimo() << std::endl;
+    std::cout << "Somma elementi: " << listaInt.somma() << std::endl;
+    std::cout << "L'elemento 1 é " << (if listaInt.contiene(1) ? "presente" : "non presente") << std::endl;
+    std::cout << "L'elemento 1 é presente : " << listaInt.contaOccorrenze(1) << std::endl;
+    
     return 0;
 }
